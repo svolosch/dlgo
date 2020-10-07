9617b320effe4d1ca4ec6878731d7f0229a108b7
Bulohov Evgeny <Bulohov@corp.eubank.com.ru>
2020-10-07 15:11:05 +0300

    str 55

--- goboard_slow.py
+++ goboard_slow.py
@@ -60,13 +60,54 @@ class Board():
         self.num_cols = num_cols
         self._grid = {}
 
+
+    def is_on_grid(self, point):
+        return 1 <= point.row <= self.num_rows and\
+            1 <= point.col <= self.num_cols
+
+    def get(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
+
+    def get_go_string(self, point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+
     def place_stone(self, player, point):
         assert self.is_on_grid(point)
         assert self._grid.get(point) is None
-        adjacent_same_color = []
-        adjacent_opposite_color = []
-        liberties = []
-        for neghbor in point.neighbors():
-            if not self.is_on_grid(neighbor):
+        adjacent_same_color = []#смежные "свои" камни
+        adjacent_opposite_color = []#смежные "чужие" камни
+        liberties = []#свободные "точки"!?
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):#точка не на доске
                 continue
+            neighbor_string = self._grid.get(neighbor)
+            if neighbor_string is None: #если клетка не занята, добавляем в список 
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+        new_string = GoString(player, [point], liberties)
+
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+
+             
+
             
